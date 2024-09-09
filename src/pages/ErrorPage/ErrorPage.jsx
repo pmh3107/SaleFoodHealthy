282b9cceb9d61abbf659353589ba26b1b047f9e6
Hiển Phan <87771924+pmh3107@users.noreply.github.com>
2024-09-09 13:08:01 +0700

    FIX: FreshFood version1.2

--- src/pages/ErrorPage/ErrorPage.jsx
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -1,17 +1,20 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
-
+import { useEffect } from "react";
 function ErrorPage() {
+	useEffect(() => {
+		window.scrollTo(0, 0);
+	}, []);
 	return (
 		<>
 			<header></header>
-			<main className="w-screen h-screen  bg-slate-200/70 flex flex-col items-center justify-center">
+			<main className="w-screen h-screen bg-white flex flex-col items-center justify-center">
 				<div className="flex items-center">
 					<div className="relative">
 						<img src="/images/ErrorPage.png" alt="" />
 						<a
 							href="/"
-							className="absolute -right-20 bottom-0 bg-[#FCD980] hover:bg-yellow-200 px-10 py-3 rounded-md text-[#282938] text-lg font-bold flex gap-2 justify-center items-center"
+							className="absolute -right-20 bottom-0 bg-[#FC8019] hover:bg-[#FFEDD0] px-10 py-3 rounded-md text-[#202020] text-lg font-bold flex gap-2 justify-center items-center"
 						>
 							<FontAwesomeIcon icon={faArrowLeft} className="" />
 							Back to Home Page
